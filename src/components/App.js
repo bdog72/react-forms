8eabce3573872e3ef71494429d78fb98d1b833cc
Brian Bycynski <high5bri@hotmail.com>
2017-06-30 06:10:56 -0400

    added another example

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,27 @@ import BasicInput03 from './03BasicInput'
 import BasicInput04 from './04BasicInput'
 import BasicInput05 from './05BasicInput'
 import StateInputMulti from './06StateInputMulti'
+import BasicValidation07 from './07BasicValidation'
 
 class App extends Component {
   render () {
-    return <div>
-      <BasicButton01 />
-      <hr />
-      <BasicButton02 />
-      <hr />
-      <BasicInput03 />
-      <hr />
-      <BasicInput04 />
-      <hr />
-      <BasicInput05 />
-      <hr />
-      <StateInputMulti />
-    </div>
+    return (
+      <div className='flex'>
+        <BasicButton01 />
+        <hr />
+        <BasicButton02 />
+        <hr />
+        <BasicInput03 />
+        <hr />
+        <BasicInput04 />
+        <hr />
+        <BasicInput05 />
+        <hr />
+        <StateInputMulti />
+        <hr />
+        <BasicValidation07 />
+      </div>
+    )
   }
 }
 
